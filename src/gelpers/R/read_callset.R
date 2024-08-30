a1f058e8e97203b9af29d6a96935caea426b7c24
Justin Lim <jlim21@mgh.harvard.edu>
2024-08-30 12:46:01 -0400

    use data.table::fread to read callset

--- src/gelpers/R/read_callset.R
+++ src/gelpers/R/read_callset.R
@@ -1,39 +1,33 @@
 #' Read a gCNV callset
 #'
 #' @description
-#' Read a callset produced by the gCNV pipeline and ensure that it has the
-#' required columns for workflows in this package.
+#' Read a callset produced by the gCNV pipeline and check for required columns.
 #'
 #' @details
-#' Many of the functions in this package that operate on a gCNV callset
-#' \code{data.table} assume that there are certain columns available because
-#' the columns are required for the function to work. The most common ones are
-#' "chr", "start", "end", "sample", "batch", "svtype", and "variant_name". gCNV
-#' should include all of these columns as part of its output, but sometimes
-#' column names change or callset processing drops some columns. Therefore,
-#' \code{read_callset()} checks for these columns and throws an error if any
-#' are missing.
-#'
-#' The callset should be a tab-delimited file with a header line. The file can
-#' be compressed, but then the \pkg{R.utils} package is required to read it.
+#' There are a few columns that every gCNV callset should have in order to do
+#' any useful analysis on it. As the gCNV pipeline changes and postprocessing
+#' is done on callsets, the columns can change as well. To ensure that a
+#' callset has these required columns, we check that they exist after reading
+#' the file (which is a tab-delimited file). Currently the required columns are
+#' "chr", "start", "end", "sample", "batch", "svtype", "variant_name", and
+#' "CN". If any of these columns don't exist, an error will be signaled.
+#' The file can be compressed with one of the compression methods supported by
+#' base R.
 #'
 #' @export
 #' @param path \code{character(1)}. Path to the callset.
-#' @returns \code{data.frame} or signals an error if required columns are
-#'   missing.
+#' @returns \code{data.table} keyed on the "sample" column.
 read_callset <- function(path) {
     assert(is_string(path))
-    x <- utils::read.table(
-        file = path,
-        sep = "\t",
-        header = TRUE
-    )
+    callset <- data.table::fread(file = path,
+                                 sep = "\t",
+                                 header = TRUE)
 
-    req_cols <- c(
-        "chr", "start", "end", "sample", "batch", "svtype", "variant_name", "CN"
-    )
-    assert(df_has_columns(x, .cols = req_cols))
-    idx <- order(x$sample)
+    req_cols <- c("chr", "start", "end", "sample", "batch", "svtype",
+                  "variant_name", "CN")
+    assert(df_has_columns(callset, .cols = req_cols))
 
-    x[idx, ]
+    data.table::setkey(callset, sample)
+
+    callset
 }
